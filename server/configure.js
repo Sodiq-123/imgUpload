77a13817abce6f16861579ced836dd119589635a
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-04-27 07:19:44 +0100

    Successfully ran the node server and added route to test image upload

---
+++ server/configure.js
@@ -0,0 +1,35 @@
+var path = require('path'),
+    routes = require('./routes'),
+    exphbs = require('express-handlebars'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    morgan = require('morgan'),
+    methodOverride = require('method-override'),
+    errorHandler = require('errorhandler'),
+    moment = require('moment'),
+    multer = require('multer');
+
+module.exports = function(app) {
+    app.engine('handlebars', exphbs.create({
+        defaultLayout: 'main',
+        layoutsDir: app.get('views') + '/layouts',
+        partialsDir: [app.get('views') + '/partials']
+    }).engine);
+    app.set('view engine', 'handlebars');
+
+    app.use(morgan('dev'));
+    var storage = multer.diskStorage({ dest: path.join(__dirname, 'public/upload/temp')});
+    app.use(multer({storage: storage}).single('file'));
+
+    app.use(methodOverride());
+    app.use(cookieParser('2729298RE(#(###(3jdkjhwjh%$(3#80jnf2i4Vrg%ty984'));
+    routes(app);
+
+    app.use('/public/', express.static(path.join(__dirname, '../public')));
+
+    if ('development' === app.get('env')) {
+        app.use(errorHandler);
+    }
+    return app;
+};
