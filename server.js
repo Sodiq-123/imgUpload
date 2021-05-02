4d1328b72a39fc3f42e01431aa9edf3f02fb9f8f
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-05-02 17:38:41 +0100

    Added Mongoose connection to the server

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ var express = require('express'),
 config = require('./server/configure'),
 path = require('path'),
 app = express();
+mongoose = require('mongoose')
 
 app.set('port', process.env.PORT || 3300);
-// app.set('views', __dirname + '/views');
 app.set('views', path.join(__dirname, 'views'));
 app = config(app);
+
+mongoose.connect('mongodb://localhost/imgUpload', {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connection.on('open', function() {
+    console.log('Mongoose Connected Successfully!');
+});
 
 
 var server = app.listen(app.get('port'), function() {
